47ddf80095c091df08b1f3784e6c58044a0b2339
nobaboy <84668101+nobaboy@users.noreply.github.com>
2025-05-03 16:39:24 +0300

    Configure dunst and wlogout, and more css cleanup

--- .config/waybar/scripts/system-monitor.sh
+++ .config/waybar/scripts/system-monitor.sh
@@ -1,1 +1,16 @@
-kitty --class system-monitor -e htop
+launch_monitor() {
+    kitty --class floating-kitty -e htop
+}
+
+if [[ "$1" == "stop" ]]; then
+    killall htop
+elif [[ "$1" == "toggle" ]]; then
+    if pgrep -x "htop" >/dev/null; then
+        killall htop
+    else
+        launch_monitor
+    fi
+else
+    launch_monitor
+    fi
+fi
